6878f654af86a98ac4946e69b7e54115b9a32331
nimash eshan <81243419+nimash3eshan@users.noreply.github.com>
2022-05-15 03:50:04 +0530

    added adjustments slide effect

--- trans.js
+++ trans.js
@@ -9,9 +9,52 @@ $(document).ready(() => {
     $('button.adj').on('click', () => {
         $('.todo').show("slow");
     });
-    $('button.adjhide').on('click', () => {
+
+    //hide adjustments
+    $('button.adjhideall').on('click', () => {
         $('.todo').hide("slow");
     });
+
+
+    //when user select a adjustment type :
+    $('.adjhide').hide();
+    $('select.tbtype').change(function(){
+        var selectedAdj = $(this).children("option:selected").val();
+        //alert(selectedAdj);
+        if(selectedAdj == 'accu'){
+            $('.adjhide').hide();
+            $('.accu-area').slideDown();
+        }
+        else if(selectedAdj == 'prepay'){
+            $('.adjhide').hide();
+            $('.prepay-area').slideDown();
+        }
+        else if(selectedAdj == 'inrec'){
+            $('.adjhide').hide();
+            $('.inrec-area').slideDown();
+        }
+        else if(selectedAdj == 'accu'){
+            $('.adjhide').hide();
+            $('.accu-area').slideDown();
+        }
+        else if(selectedAdj == 'inad'){
+            $('.adjhide').hide();
+            $('.inad-area').slideDown();
+        }
+        else if(selectedAdj == 'baddebt'){
+            $('.adjhide').hide();
+            $('.baddebt-area').slideDown();
+        }
+        else if(selectedAdj == 'prodebt'){
+            $('.adjhide').hide();
+            $('.prodebt-area').slideDown();
+        }
+        else {
+            $('.adjhide').hide();
+            $('.deprec-area').slideDown();
+        }
+
+    })
 
     })
 

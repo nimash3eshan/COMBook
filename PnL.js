89a3c17dfd3b910f4167df4115c52d4b61bd8aac
milanswanthra <86518863+milanswanthra@users.noreply.github.com>
2022-05-25 01:55:32 +0530

    Setup the adjustment for retrieving accruals

--- PnL.js
+++ PnL.js
@@ -57,14 +57,14 @@ let total2 = 0;
 colRecords.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
         let obj = {
-            ID : doc.id ,
-            Date: doc.data().dateAndTime ,
-            description: doc.data().title ,
-            Type: doc.data().assetType ,
-            CrDb: doc.data().creditDebit ,
-            Exp_Type: (doc.data().exp_type == "")? "-":doc.data().exp_type ,
-            Ass_Type: (doc.data().ass_type == "")? "-":doc.data().ass_type ,
-            Liab_Type: (doc.data().liab_type == "")? "-":doc.data().liab_type ,
+            ID: doc.id,
+            Date: doc.data().dateAndTime,
+            description: doc.data().title,
+            Type: doc.data().assetType,
+            CrDb: doc.data().creditDebit,
+            Exp_Type: (doc.data().exp_type == "") ? "-" : doc.data().exp_type,
+            Ass_Type: (doc.data().ass_type == "") ? "-" : doc.data().ass_type,
+            Liab_Type: (doc.data().liab_type == "") ? "-" : doc.data().liab_type,
             Amount: doc.data().amount
         };
         records.push(obj);
@@ -159,18 +159,27 @@ async function getData() {
 
 function administrative() {
     let total = 0;
-    let filtered = records.filter(e =>{return ((e.Type === "expense") && (e.Exp_Type ==="ad-ex"))})
+    let filtered = records.filter(e => {
+        return ((e.Type === "expense") && (e.Exp_Type === "ad-ex"))
+    })
     filtered.forEach((r, idx, array) => {
+        let rec = accruals.filter(e => {
+            return ((e.ID === r.ID))
+        })
+        if (rec.length !== 0) {
+            rec.forEach((f) => {
+                r.Amount += r.Amount + f.Amount;
+            })
+        }
         total += r.Amount;
-        if(Object.is(array.length - 1, idx)) {
+        if (Object.is(array.length - 1, idx)) {
             document.getElementById("administrative").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
                 '                            <td class="even" id="wagesSalaries">' + r.Amount + '</td>\n' +
-                '                            <td class="even" id="TotalAdminstrativeExpenses"> '+total+' </td>'+
+                '                            <td class="even" id="TotalAdminstrativeExpenses"> ' + total + ' </td>' +
                 '                        </tr>';
             total2 += total;
-        }
-        else {
+        } else {
 
             document.getElementById("administrative").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
@@ -179,20 +188,30 @@ function administrative() {
         }
     });
 }
+
 function distributing() {
     let total = 0;
-    let filtered = records.filter(e =>{return ((e.Type === "expense") && (e.Exp_Type ==="dns-ex"))})
+    let filtered = records.filter(e => {
+        return ((e.Type === "expense") && (e.Exp_Type === "dns-ex"))
+    })
     filtered.forEach((r, idx, array) => {
+        let rec = accruals.filter(e => {
+            return ((e.ID === r.ID))
+        })
+        if (rec.length !== 0) {
+            rec.forEach((f) => {
+                r.Amount += f.Amount;
+            })
+        }
         total += r.Amount;
-        if(Object.is(array.length - 1, idx)) {
+        if (Object.is(array.length - 1, idx)) {
             document.getElementById("distributing").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
                 '                            <td class="even" id="wagesSalaries">' + r.Amount + '</td>\n' +
-                '                            <td class="even" id="TotalAdminstrativeExpenses"> '+total+' </td>'+
+                '                            <td class="even" id="TotalAdminstrativeExpenses"> ' + total + ' </td>' +
                 '                        </tr>';
             total2 += total;
-        }
-        else {
+        } else {
 
             document.getElementById("distributing").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
@@ -201,21 +220,31 @@ function distributing() {
         }
     });
 }
+
 function financialExpenses() {
     let total = 0;
-    let filtered = records.filter(e =>{return ((e.Type === "expense") && (e.Exp_Type ==="f-ex"))})
+    let filtered = records.filter(e => {
+        return ((e.Type === "expense") && (e.Exp_Type === "f-ex"))
+    })
     filtered.forEach((r, idx, array) => {
+        let rec = accruals.filter(e => {
+            return ((e.ID === r.ID))
+        })
+        if (rec.length !== 0) {
+            rec.forEach((f) => {
+                r.Amount += f.Amount;
+            })
+        }
         total += r.Amount;
-        if(Object.is(array.length - 1, idx)) {
+        if (Object.is(array.length - 1, idx)) {
             document.getElementById("financialExpenses").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
                 '                            <td class="even" id="wagesSalaries">' + r.Amount + '</td>\n' +
-                '                            <td class="even" id="TotalDistributingExpenses"> '+total+' </td>'+
+                '                            <td class="even" id="TotalDistributingExpenses"> ' + total + ' </td>' +
                 '                        </tr>';
             total2 += total;
 
-        }
-        else {
+        } else {
 
             document.getElementById("financialExpenses").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
@@ -224,21 +253,31 @@ function financialExpenses() {
         }
     });
 }
+
 function other() {
     let total = 0;
-    let filtered = records.filter(e =>{return ((e.Type === "expense") && (e.Exp_Type ==="f-ex"))})
+    let filtered = records.filter(e => {
+        return ((e.Type === "expense") && (e.Exp_Type === "f-ex"))
+    })
     filtered.forEach((r, idx, array) => {
+        let rec = accruals.filter(e => {
+            return ((e.ID === r.ID))
+        })
+        if (rec.length !== 0) {
+            rec.forEach((f) => {
+                r.Amount += f.Amount;
+            })
+        }
         total += r.Amount;
-        if(Object.is(array.length - 1, idx)) {
+        if (Object.is(array.length - 1, idx)) {
             document.getElementById("other").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
                 '                            <td class="even" id="wagesSalaries">' + r.Amount + '</td>\n' +
-                '                            <td class="even" id="TotalDistributingExpenses"> '+total+' </td>'+
+                '                            <td class="even" id="TotalDistributingExpenses"> ' + total + ' </td>' +
                 '                        </tr>';
             total2 += total;
 
-        }
-        else {
+        } else {
 
             document.getElementById("other").innerHTML += '<tr>\n' +
                 '                            <td>' + r.description + '</td>\n' +
@@ -248,27 +287,31 @@ function other() {
     });
 }
 
-function sales_set(){
+function sales_set() {
     let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'sales')||(w.description.toLowerCase() === 'sale'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+    const filtered = records.filter(w => {
+        return ((w.description.toLowerCase() === 'sales') || (w.description.toLowerCase() === 'sale'))
     })
-    total2+=total;
+    filtered.forEach((f) => {
+        total += f.Amount;
+    })
+    total2 += total;
     document.getElementById('sales').innerHTML = total;
 }
 
-function ret_inw_set(){
+function ret_inw_set() {
     let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'return inward')||(w.description.toLowerCase() === 'return inwards')||(w.description.toLowerCase() === 'sales return'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+    const filtered = records.filter(w => {
+        return ((w.description.toLowerCase() === 'return inward') || (w.description.toLowerCase() === 'return inwards') || (w.description.toLowerCase() === 'sales return'))
+    })
+    filtered.forEach((f) => {
+        total += f.Amount;
     })
     total2 -= total;
     document.getElementById('returnInwards').innerHTML = total;
 }
 
-function sales_tot_set(){
+function sales_tot_set() {
     document.getElementById('SalesTotal').innerHTML = total2;
 }
 
@@ -324,16 +367,19 @@ function sales_tot_set(){
 
 function set_cost_of_sales() {
     const tbody = document.getElementById('costofsales');
-    const filtered = records.filter(w => {return ((w.Type === 'expense')&&(w.Exp_Type == 'cos-ex'))})
-    filtered.forEach((f)=>{
-        total2+=f.Amount;
-        tbody.insertRow().innerHTML += '<tr>'+
-            '<td>'+f.description+'</td>'+
-            '<td class="even" id="Cash">'+f.Amount+'</td>'+
+    const filtered = records.filter(w => {
+        return ((w.Type === 'expense') && (w.Exp_Type == 'cos-ex'))
+    })
+    filtered.forEach((f) => {
+        total2 += f.Amount;
+        tbody.insertRow().innerHTML += '<tr>' +
+            '<td>' + f.description + '</td>' +
+            '<td class="even" id="Cash">' + f.Amount + '</td>' +
             '</tr>'
     })
 }
-function cos_of_sales_set(){
+
+function cos_of_sales_set() {
     document.getElementById('TotalCostOfSales').innerHTML = total2;
 }
 
@@ -357,22 +403,25 @@ function cos_of_sales_set(){
 //     document.getElementById('rentincome').innerHTML = total;
 // }
 
-function set_income(){
+function set_income() {
     const tbody = document.getElementById('income');
-    const filtered = records.filter(w => {return ((w.Type === 'income'))})
-    filtered.forEach((f)=>{
-        total2+=f.Amount;
-        tbody.insertRow().innerHTML += '<tr>'+
-            '<td>'+f.description+'</td>'+
-            '<td class="even" id="Cash">'+f.Amount+'</td>'+
+    const filtered = records.filter(w => {
+        return ((w.Type === 'income'))
+    })
+    filtered.forEach((f) => {
+        total2 += f.Amount;
+        tbody.insertRow().innerHTML += '<tr>' +
+            '<td>' + f.description + '</td>' +
+            '<td class="even" id="Cash">' + f.Amount + '</td>' +
             '</tr>'
     })
 }
-function TotalIncome(){
+
+function TotalIncome() {
     document.getElementById('TotalIncome').innerHTML = total2;
 }
 
-function set_profits(){
+function set_profits() {
     document.getElementById('TotalGrossProfit').innerHTML = (parseInt(document.getElementById('SalesTotal').textContent) - parseInt(document.getElementById('TotalCostOfSales').textContent)).toString();
     document.getElementById('netprofit').innerHTML = (parseInt(document.getElementById('TotalGrossProfit').textContent) + parseInt(document.getElementById('TotalIncome').textContent) - parseInt(document.getElementById('TotalExpenses').textContent)).toString();
     db.collection("homepage").doc("dashboard").set({

06a4f0cc304c677c8511c96fe1497d95b1e093ac
milanswanthra <86518863+milanswanthra@users.noreply.github.com>
2022-05-24 23:51:43 +0530

    Setup automated record retrieval from firabse

--- fps.js
+++ fps.js
@@ -37,10 +37,9 @@ const Goodwill = document.getElementById("Goodwill");
 const WebsiteAndDomains = document.getElementById("WebsiteAndDomains");
 const OtherIntangibleAssets = document.getElementById("OtherIntangibleAssets");
 const TotalNonCurrentAssets = document.getElementById("TotalNonCurrentAssets");
-const Equity = document.getElementById("Equity");
 // const shareCapital = document.getElementById("ShareCapital");
 const netProfit = document.getElementById("netProfit");
-const Reserves = document.getElementById("equity2");
+const Equity = document.getElementById("equity2");
 const TotalEquity = document.getElementById("TotalEquity");
 const TotalAssets = document.getElementById("TotalAssets");
 const TotalLiabilitiesAndEquity = document.getElementById("TotalLiabilitiesAndEquity");
@@ -61,36 +60,25 @@ colRecords.get().then((querySnapshot) => {
             Date: doc.data().dateAndTime ,
             description: doc.data().title ,
             Type: doc.data().assetType ,
+            CrDb: doc.data().creditDebit ,
             Exp_Type: (doc.data().exp_type == "")? "-":doc.data().exp_type ,
+            Ass_Type: (doc.data().ass_type == "")? "-":doc.data().ass_type ,
+            Liab_Type: (doc.data().liab_type == "")? "-":doc.data().liab_type ,
             Amount: doc.data().amount
         };
         records.push(obj);
     });
     getData().then(r =>{
-        cash_set();
-        sht_trm_set();
-        acc_recei_set();
-        pre_rent_set();
-        inve_set();
+        set_current_asset();
         tot_c_asst_set();
         total2=0;
-        land_set();
-        ppe_set();
-        // afd_set();
-        or_set();
-        gw_set();
-        wad_set();
-        oia_set();
+        set_non_current_asset();
         tot_nc_asst_set();
         total2 = 0;
-        ap_set();
-        stl_set();
-        ae_set();
+        set_current_liabilities();
         tot_c_liab_set();
         total2 = 0;
-        ltl_set();
-        bp_set();
-        dt_set();
+        set_non_current_liabilities();
         tot_nc_liab_set();
         tot_liab_set();
         total2 = 0;
@@ -178,56 +166,20 @@ async function getData() {
 }
 
 
-function cash_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'cash'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2+=total;
-    cash.innerHTML = total;
-}
 
-function sht_trm_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'short term investment'))})
-    console.log(records)
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+
+
+
+function set_current_asset(){
+    const tbody = document.getElementById('current assets');
+    const filtered = records.filter(w => {return ((w.Type === 'asset')&(w.Ass_Type === 'c-as'))});
+    filtered.forEach((rec)=>{
+        total2 += rec.Amount;
+        tbody.insertRow().innerHTML += '<tr>'+
+            '<td>'+rec.description+'</td>'+
+            '<td class="even" id="Cash">'+rec.Amount+'</td>'+
+        '</tr>'
     })
-    total2 += total;
-    console.log(total2)
-    ShortInvestments.innerHTML = total;
-}
-function acc_recei_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'accounts receivable'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    AccountsReceivable.innerHTML = total;
-}
-function pre_rent_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'prepaid rent'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    PrepaidRent.innerHTML = total;
-}
-function inve_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'inventory'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    Inventory.innerHTML = total;
 }
 
 function tot_c_asst_set(){
@@ -235,147 +187,48 @@ function tot_c_asst_set(){
 }
 
 
-
-function land_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'land'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+function set_non_current_asset(){
+    const tbody = document.getElementById('non current assets');
+    const filtered = records.filter(w => {return ((w.Type === 'asset')&(w.Ass_Type === 'nc-as'))});
+    filtered.forEach((rec)=>{
+        total2 += rec.Amount;
+        tbody.insertRow().innerHTML += '<tr>'+
+            '<td>'+rec.description+'</td>'+
+            '<td class="even" id="Cash">'+rec.Amount+'</td>'+
+        '</tr>'
     })
-    total2+=total;
-    Land.innerHTML = total;
-}
-
-function ppe_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'property')||(w.description.toLowerCase() === 'plant')||(w.description.toLowerCase() === 'equipment'))})
-    console.log(records)
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    Property.innerHTML = total;
-}
-// function afd_set(){
-//     let total = 0;
-//     const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'allowance for depreciation'))})
-//     filtered.forEach((f)=>{
-//         total+=f.Amount;
-//     })
-//     total2 += total;
-//     console.log(total2)
-//     Allowance.innerHTML = total;
-// }
-function or_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'other receivable'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    OtherReceivables.innerHTML = total;
-}
-function gw_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'goodwill'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    Goodwill.innerHTML = total;
-}
-function wad_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'website')||(w.description.toLowerCase() === 'domain'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    WebsiteAndDomains.innerHTML = total;
-}
-function oia_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'other intangible assets'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    OtherIntangibleAssets.innerHTML = total;
 }
 
 function tot_nc_asst_set(){
     TotalNonCurrentAssets.innerHTML = total2;
 }
 
-function ap_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'accounts payable'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+function set_current_liabilities(){
+    const tbody = document.getElementById('current liabilities');
+    const filtered = records.filter(w => {return ((w.Type === 'liab')&(w.Liab_Type === 'c-lia'))});
+    filtered.forEach((rec)=>{
+        total2 += rec.Amount;
+        tbody.insertRow().innerHTML += '<tr>'+
+            '<td>'+rec.description+'</td>'+
+            '<td class="even" id="Cash">'+rec.Amount+'</td>'+
+            '</tr>'
     })
-    total2 += total;
-    console.log(total2)
-    AccountsPayable.innerHTML = total;
-}
-function stl_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'short term loan'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    ShortTermLoans.innerHTML = total;
-}
-function ae_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'accrued expenses'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    AccruedExpenses.innerHTML = total;
 }
 
 function tot_c_liab_set(){
     TotalCurrentLiabilities.innerHTML = total2;
 }
 
-function ltl_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'long term loan'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
+function set_non_current_liabilities(){
+    const tbody = document.getElementById('non current liabilities');
+    const filtered = records.filter(w => {return ((w.Type === 'liab')&(w.Liab_Type === 'nc-lia'))});
+    filtered.forEach((rec)=>{
+        total2 += rec.Amount;
+        tbody.insertRow().innerHTML += '<tr>'+
+            '<td>'+rec.description+'</td>'+
+            '<td class="even" id="Cash">'+rec.Amount+'</td>'+
+            '</tr>'
     })
-    total2 += total;
-    console.log(total2)
-    LongTermLoans.innerHTML = total;
-}
-function bp_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'bond payable'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    BondsLoans.innerHTML = total;
-}
-function dt_set(){
-    let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'deferred tax'))})
-    filtered.forEach((f)=>{
-        total+=f.Amount;
-    })
-    total2 += total;
-    console.log(total2)
-    DeferredTax.innerHTML = total;
 }
 
 function tot_nc_liab_set(){
@@ -385,30 +238,30 @@ function tot_liab_set(){
     TotalLiabilities.innerHTML = (parseInt(TotalNonCurrentLiabilities.innerHTML)+parseInt(TotalCurrentLiabilities.innerHTML)).toString();
 }
 
-
 function np_set(){
     total2 += parseInt(NetProfitFB);
-    console.log(NetProfitFB)
     netProfit.innerHTML = NetProfitFB;
 }
 function eq_set(){
     let total = 0;
-    const filtered = records.filter(w => {return ((w.description.toLowerCase() === 'equity'))})
+    const filtered = records.filter(w => {return ((w.Type.toLowerCase() === 'capital'))})
     filtered.forEach((f)=>{
-        total+=f.Amount;
+        (f.CrDb =='credit')? total+=f.Amount: total-= f.Amount;
     })
     total2 += total;
-    console.log(total2)
     Equity.innerHTML = total;
 }
 
 function tot_oe_set(){
     TotalEquity.innerHTML = total2;
 }
-
-
 function totals_set(){
     TotalAssets.innerHTML = (parseInt(TotalCurrentAssets.innerHTML)+parseInt(TotalNonCurrentAssets.innerHTML)).toString();
     TotalLiabilitiesAndEquity.innerHTML = (parseInt(TotalLiabilities.innerHTML)+parseInt(TotalEquity.innerHTML)).toString();
 }
+
+
+
+
+
 
